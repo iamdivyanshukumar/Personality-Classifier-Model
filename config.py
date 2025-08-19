1addaed8e39ecf185e1b13ba921f4108d859711c
Divyanshu <divyanshussa@gmail.com>
2025-08-20 01:43:07 +0530

    rename run.py to app.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+import secrets
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
     TEMPLATES_AUTO_RELOAD = True
